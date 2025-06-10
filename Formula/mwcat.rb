a3271937c103342221e6110e6688b2b59467c30d
Jesse Kempf <jessekempf@gmail.com>
2025-06-10 15:04:11 -0700

    Package mrwcat v0.1.2-pre1

--- Formula/mwcat.rb
+++ Formula/mwcat.rb
@@ -1,8 +1,8 @@
 class Mwcat < Formula
   desc "Mister Webhooks console consumer"
   homepage "https://github.com/mister-webhooks/mwcat"
-  url "https://github.com/mister-webhooks/mwcat/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "6f98a58f9156eeff1b2a1b2e9a055f65b1bbceeaad4a757a394243ad6203ae6e"
+  url "https://github.com/mister-webhooks/mwcat/archive/refs/tags/v0.1.2-pre1.tar.gz"
+  sha256 "0eda98ae4ac3041e6ed8f606a21ec4dcca8af6113c2f1eb36bc68297b271f4c9"
   license "BSD-2-Clause"
 
   depends_on "go" => :build
