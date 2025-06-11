acb542246f6fe03abda84c7bf1ffbc9ed9372481
Jesse Kempf <361220+jessekempf@users.noreply.github.com>
2025-06-11 20:00:44 +0000

    mwtail: update 0.3.0-pre0 bottle.

--- Formula/mwtail.rb
+++ Formula/mwtail.rb
@@ -6,11 +6,11 @@ class Mwtail < Formula
   license "BSD-2-Clause"
 
   bottle do
-    root_url "https://github.com/mister-webhooks/homebrew-tools/releases/download/mwcat-0.2.0-pre0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9bea5184030e35b9a2e5519049531292d4664881210073ce97c17a1b191e989a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "397a881658109998516c0905c443a136d6e7df73bcbad1804254a25aa9f8a1dd"
-    sha256 cellar: :any_skip_relocation, ventura:       "48d3c21e84fe9fc5e98124f70b14a6a552e681291bbe03b4b89ec719f0aa00d5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "50cfdee6d06cdeee3477e5292eb9e13020db6f55cbb7f5f25917695edf900cfa"
+    root_url "https://github.com/mister-webhooks/homebrew-tools/releases/download/mwtail-0.3.0-pre0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2b7308cf1bb05ed7622700a9f22f9f90e49aeeafcfcc1685b283629c129a8d3e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5f5191542c92c03114b83132b955f093a3b6d3155968595dc8aa578598395a44"
+    sha256 cellar: :any_skip_relocation, ventura:       "861ecb4d7bcaee89c39077559dbe39fafe851cda30fbe5a135f1d780c80ab9bc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "25c8d18e944c2285e1b6eb2b5061828a076d8cc12f5ce4fb5a577a929e061a0b"
   end
 
   depends_on "go" => :build
